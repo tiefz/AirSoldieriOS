dd93e284d4efdfc06f06bd3178126e7e6b616cfd
Daniel Tief <chinorex@gmail.com>
2023-08-30 20:57:31 -0300

    Initial Commit

---
+++ AirSoldier/AirSoldierApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirSoldierApp.swift
+//  AirSoldier
+//
+//  Created by Daniel Batista on 30/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AirSoldierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirSoldier/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AirSoldier
+//
+//  Created by Daniel Batista on 30/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
